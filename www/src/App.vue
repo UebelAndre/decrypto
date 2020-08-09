4817b3fece24027749cb8e234a3f767ab08a64a2
Andre Brisco <andre.brisco@gmail.com>
2020-08-08 18:34:25 -0700

    Bazel conversion and added 'Hello Vue' app

---
+++ www/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+const getWasm = () => import("decrypto-wasm")
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            msg: getWasm().then(wasm => wasm.hello_world())
+        };
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
